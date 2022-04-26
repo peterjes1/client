ece340cd21fbe5a3c90e79e6fd7f517ac1788a54
peterjes1 <peterjesus11040@gmail.com>
2022-04-26 07:57:24 +0530

    commit

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { signIn, signOut } from '../actions';
+import { connect } from 'react-redux';
+
+class GoogleAuth extends React.Component{
+
+    
+
+    componentDidMount(){
+        window.gapi.load('client:auth2', ()=>{
+            window.gapi.client.init(
+                {clientId: '1481516985-91rfdc1q65am7h1ku5jln80ojrksgpd4.apps.googleusercontent.com',
+                scope : 'email'
+            }
+            ).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+               this.onAuthChange(this.auth.isSignedIn.get());
+                this.auth.isSignedIn.listen(this.onAuthChange)
+            })
+        });
+    }
+
+    onAuthChange = (isSignedIn) =>{
+        if(isSignedIn){
+            this.props.signIn(this.auth.currentUser.get().getId());
+        }
+        else{
+            this.props.signOut();
+        }
+    }
+
+    onSignInClick =() => {
+        this.auth.signIn();
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
+
+    renderAuthButton(){
+        if(this.props.isSignedIn===null){
+            return null
+        }
+        else if(this.props.isSignedIn){
+            return (
+                <button className='ui red google button' onClick={this.onSignOutClick}>
+                    <i className='google icon' />
+                    Sign Out
+                </button>
+            )
+        }
+        else{
+            return (
+                <button className='ui red google button' onClick={this.onSignInClick}>
+                    <i className='google icon' />
+                    Sign in with google
+                </button>
+            )
+        }
+    }
+
+    
+    
+    render(){
+        return(
+            <div>
+                {this.renderAuthButton()}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {isSignedIn : state.auth.isSignedIn};
+};
+
+export default connect(mapStateToProps,{signIn,signOut})(GoogleAuth);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store=createStore(
+    reducers,
+    composeEnhancer(applyMiddleware())
+    );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+    
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
